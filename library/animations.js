0286fbb5b523a2876389f42bfba956d93eb123e7
Sacha Beaumier <sacha.beaumier@gmail.com>
2023-02-04 21:30:31 +1300

    added new js stuff to page

---
+++ library/animations.js
@@ -0,0 +1,20 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight)
+
+  // angleMode(DEGREES)
+  // rectMode(CENTER)
+}
+
+function draw() {
+  // background(10, 20, 30)
+  if (mouseIsPressed) {
+    fill(112, 107, 148) // 150
+  } else {
+    fill(143, 148, 107) //255
+  }
+  ellipse(mouseX, mouseY, 800, 800)
+}
+
+// 234, 21, 146
+
+// 21, 234, 109
---
+++ library/newanimations.js
@@ -0,0 +1,30 @@
+
+int cols, rows;
+int scl = 20;
+
+void setup() {
+  // createCanvas(windowWidth, windowHeight)
+
+  // angleMode(DEGREES)
+
+  ResizeObserverSize(600, 600, P3D)
+}
+
+// function draw() {
+//   // background(30)
+//   // rotateX(60)
+//   // noFill()
+//   // stroke(255)
+// }
+
+void draw() {
+
+background(0)
+
+for (int x = 0; x < cols; x++) {
+  for (int y = 0; y < rows; y++) {
+
+  }
+
+}
+}

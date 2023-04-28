c72015a47471600694e8aab6b87f276693c50cd2
Sacha Beaumier <sacha.beaumier@gmail.com>
2023-04-28 19:12:35 +1200

    updated changes to portfolio site

--- library/animations.js
+++ library/animations.js
@@ -5,100 +5,176 @@
 //   // rectMode(CENTER)
 // }
 
-// function draw() {
-//   // background(10, 20, 30)
-//   if (mouseIsPressed) {
-//     fill(112, 107, 148) // 150
-//   } else {
-//     fill(143, 148, 107) //255
+// let numBalls = 13
+// let spring = 0.1
+// let gravity = 0.01
+// let friction = -0.9
+// let balls = []
+
+// function setup() {
+//   createCanvas(windowWidth, windowHeight)
+//   for (let i = 0; i < numBalls; i++) {
+//     balls[i] = new Ball(random(width), random(height), random(30, 70), i, balls)
 //   }
-//   ellipse(mouseX, mouseY, 400, 400)
+//   noStroke()
+//   fill(255, 204)
 // }
 
-// 234, 21, 146
+// function draw() {
+//   background(150, 150)
+//   balls.forEach((ball) => {
+//     ball.collide()
+//     ball.move()
+//     ball.display()
+//   })
+// }
 
-// 21, 234, 109
+// class Ball {
+//   constructor(xin, yin, din, idin, oin) {
+//     this.x = xin
+//     this.y = yin
+//     this.vx = 0
+//     this.vy = 0
+//     this.diameter = din
+//     this.id = idin
+//     this.others = oin
+//   }
 
-let numBalls = 13
-let spring = 0.1
-let gravity = 0.01
-let friction = -0.9
-let balls = []
+//   collide() {
+//     for (let i = this.id + 1; i < numBalls; i++) {
+//       // console.log(others[i]);
+//       let dx = this.others[i].x - this.x
+//       let dy = this.others[i].y - this.y
+//       let distance = sqrt(dx * dx + dy * dy)
+//       let minDist = this.others[i].diameter / 2 + this.diameter / 2
+//       //   console.log(distance);
+//       //console.log(minDist);
+//       if (distance < minDist) {
+//         //console.log("2");
+//         let angle = atan2(dy, dx)
+//         let targetX = this.x + cos(angle) * minDist
+//         let targetY = this.y + sin(angle) * minDist
+//         let ax = (targetX - this.others[i].x) * spring
+//         let ay = (targetY - this.others[i].y) * spring
+//         this.vx -= ax
+//         this.vy -= ay
+//         this.others[i].vx += ax
+//         this.others[i].vy += ay
+//       }
+//     }
+//   }
+
+//   move() {
+//     this.vy += gravity
+//     this.x += this.vx
+//     this.y += this.vy
+//     if (this.x + this.diameter / 2 > width) {
+//       this.x = width - this.diameter / 2
+//       this.vx *= friction
+//     } else if (this.x - this.diameter / 2 < 0) {
+//       this.x = this.diameter / 2
+//       this.vx *= friction
+//     }
+//     if (this.y + this.diameter / 2 > height) {
+//       this.y = height - this.diameter / 2
+//       this.vy *= friction
+//     } else if (this.y - this.diameter / 2 < 0) {
+//       this.y = this.diameter / 2
+//       this.vy *= friction
+//     }
+//   }
+
+//   display() {
+//     ellipse(this.x, this.y, this.diameter, this.diameter)
+//   }
+// }
+
+let w
+let columns
+let rows
+let board
+let next
 
 function setup() {
+  // Set simulation framerate to 10 to avoid flickering
+  frameRate(10)
   createCanvas(windowWidth, windowHeight)
-  for (let i = 0; i < numBalls; i++) {
-    balls[i] = new Ball(random(width), random(height), random(30, 70), i, balls)
+  w = 20
+  // Calculate columns and rows
+  columns = floor(width / w)
+  rows = floor(height / w)
+  // Wacky way to make a 2D array is JS
+  board = new Array(columns)
+  for (let i = 0; i < columns; i++) {
+    board[i] = new Array(rows)
   }
-  noStroke()
-  fill(255, 204)
+  // Going to use multiple 2D arrays and swap them
+  next = new Array(columns)
+  for (i = 0; i < columns; i++) {
+    next[i] = new Array(rows)
+  }
+  init()
 }
 
 function draw() {
-  background(150, 150)
-  balls.forEach((ball) => {
-    ball.collide()
-    ball.move()
-    ball.display()
-  })
+  background(255)
+  generate()
+  for (let i = 0; i < columns; i++) {
+    for (let j = 0; j < rows; j++) {
+      if (board[i][j] == 1) fill(0)
+      else fill(255)
+      stroke(0)
+      rect(i * w, j * w, w - 1, w - 1)
+    }
+  }
 }
 
-class Ball {
-  constructor(xin, yin, din, idin, oin) {
-    this.x = xin
-    this.y = yin
-    this.vx = 0
-    this.vy = 0
-    this.diameter = din
-    this.id = idin
-    this.others = oin
-  }
+// reset board when mouse is pressed
+function mousePressed() {
+  init()
+}
 
-  collide() {
-    for (let i = this.id + 1; i < numBalls; i++) {
-      // console.log(others[i]);
-      let dx = this.others[i].x - this.x
-      let dy = this.others[i].y - this.y
-      let distance = sqrt(dx * dx + dy * dy)
-      let minDist = this.others[i].diameter / 2 + this.diameter / 2
-      //   console.log(distance);
-      //console.log(minDist);
-      if (distance < minDist) {
-        //console.log("2");
-        let angle = atan2(dy, dx)
-        let targetX = this.x + cos(angle) * minDist
-        let targetY = this.y + sin(angle) * minDist
-        let ax = (targetX - this.others[i].x) * spring
-        let ay = (targetY - this.others[i].y) * spring
-        this.vx -= ax
-        this.vy -= ay
-        this.others[i].vx += ax
-        this.others[i].vy += ay
+// Fill board randomly
+function init() {
+  for (let i = 0; i < columns; i++) {
+    for (let j = 0; j < rows; j++) {
+      // Lining the edges with 0s
+      if (i == 0 || j == 0 || i == columns - 1 || j == rows - 1) board[i][j] = 0
+      // Filling the rest randomly
+      else board[i][j] = floor(random(2))
+      next[i][j] = 0
+    }
+  }
+}
+
+// The process of creating the new generation
+function generate() {
+  // Loop through every spot in our 2D array and check spots neighbors
+  for (let x = 1; x < columns - 1; x++) {
+    for (let y = 1; y < rows - 1; y++) {
+      // Add up all the states in a 3x3 surrounding grid
+      let neighbors = 0
+      for (let i = -1; i <= 1; i++) {
+        for (let j = -1; j <= 1; j++) {
+          neighbors += board[x + i][y + j]
+        }
       }
+
+      // A little trick to subtract the current cell's state since
+      // we added it in the above loop
+      neighbors -= board[x][y]
+      // Rules of Life
+      if (board[x][y] == 1 && neighbors < 2) next[x][y] = 0 // Loneliness
+      else if (board[x][y] == 1 && neighbors > 3)
+        next[x][y] = 0 // Overpopulation
+      else if (board[x][y] == 0 && neighbors == 3)
+        next[x][y] = 1 // Reproduction
+      else next[x][y] = board[x][y] // Stasis
     }
   }
 
-  move() {
-    this.vy += gravity
-    this.x += this.vx
-    this.y += this.vy
-    if (this.x + this.diameter / 2 > width) {
-      this.x = width - this.diameter / 2
-      this.vx *= friction
-    } else if (this.x - this.diameter / 2 < 0) {
-      this.x = this.diameter / 2
-      this.vx *= friction
-    }
-    if (this.y + this.diameter / 2 > height) {
-      this.y = height - this.diameter / 2
-      this.vy *= friction
-    } else if (this.y - this.diameter / 2 < 0) {
-      this.y = this.diameter / 2
-      this.vy *= friction
-    }
-  }
-
-  display() {
-    ellipse(this.x, this.y, this.diameter, this.diameter)
-  }
+  // Swap!
+  let temp = board
+  board = next
+  next = temp
 }

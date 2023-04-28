86accb1d7eaa362d7e3af0422e9f10a38248e9d4
Sacha Beaumier <sacha.beaumier@gmail.com>
2023-04-28 14:47:55 +1200

    updating portfolio

--- library/newanimations.js
+++ library/newanimations.js
@@ -1,1 +1,8 @@
+const animation = document.querySelector('.animation')
+animation.style.left = '0'
 
+let pos = 0
+setInterval(() => {
+  pos = (pos + 1) % window.innerWidth
+  animation.style.left = `-${pos}px`
+}, 10)
